88d7696f3a86b45722170f2648d37ae0fa180122
Trisha9987 <138782760+Trisha9987@users.noreply.github.com>
2023-07-14 13:08:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,6 @@
-from flask import Flask, request
-import telegram
-import schedule
-import time
+from flask import Flask
 
 app = Flask(__name__)
-
-# Function to send the specific message to each group
-def send_specific_message_to_groups():
-    bot_token = "6135924880:AAH7jJQtLpnyTENBTqeAdgDMBtzbvaK07vw"
-    message = "Hello Everyone 👋🏻"
-
-    bot = telegram.Bot(token=bot_token)
-    groups = bot.getUpdates()  # Retrieve information about the groups the bot is a member of
-
-    for group in groups:
-        chat_id = group.effective_chat.id
-        bot.send_message(chat_id=chat_id, text=message)
-
-# Schedule the message to be sent every hour
-schedule.every(1).minutes.do(send_specific_message_to_groups)
-
-# Run the timer
-while True:
-    schedule.run_pending()
-    time.sleep(1)
 
 
 @app.route('/')
