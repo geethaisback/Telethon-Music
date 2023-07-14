4da7a783f6b57077bcf76a31209f14c7c6f39e77
Trisha9987 <138782760+Trisha9987@users.noreply.github.com>
2023-07-14 18:55:07 +0530

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -9,7 +9,7 @@ message = 'Hello from the test script!'
 # Function to send the message
 def send_message():
     bot = Bot(token=BOT_TOKEN)
-    bot.send_message(chat_id='YOUR_CHAT_ID', text=message)
+    bot.send_message(chat_id='@publictestbottc', text=message)
 
 # Call the send_message function
 send_message()
