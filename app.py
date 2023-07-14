bd3fd887fccf1ebf9774080cc80ea3d9434f53c0
Trisha9987 <138782760+Trisha9987@users.noreply.github.com>
2023-07-14 17:40:15 +0530

    Update send message every minute in app.py

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+import telegram
+import schedule
+import time
 
 app = Flask(__name__)
+
+# Function to send the specific message to each group
+def send_specific_message_to_groups():
+    bot_token = '6135924880:AAH7jJQtLpnyTENBTqeAdgDMBtzbvaK07vw'
+    message = 'Hello everyone'
+
+    bot = telegram.Bot(token=bot_token)
+    groups = bot.getUpdates()  # Retrieve information about the groups the bot is a member of
+
+    for group in groups:
+        chat_id = group.effective_chat.id
+        bot.send_message(chat_id=chat_id, text=message)
+
+# Schedule the message to be sent every minute
+schedule.every(1).minute.do(send_specific_message_to_groups)
+
+# Run the timer
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 
 @app.route('/')
