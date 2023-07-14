ab2ed5316a4c1b58c731dc9854da2c2fbf701742
Trisha9987 <138782760+Trisha9987@users.noreply.github.com>
2023-07-14 19:20:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,5 @@
-from flask import Flask, request
-import schedule
-import time
-import telegram
-
+from flask import Flask
 app = Flask(__name__)
-
-bot_token = '6135924880:AAH7jJQtLpnyTENBTqeAdgDMBtzbvaK07vw'
-bot = telegram.Bot(token=bot_token)
-
-# Handler for the /run command
-@app.route('/run', methods=['POST'])
-def run_command():
-    message = request.get_json()['message']
-    command = message.get('text', '').lower()
-
-    # Check if the command is /run
-    if command == '/run':
-        # Execute your desired functionality or code here
-        # ...
-        # Example: Send a response message
-        chat_id = message['chat']['id']
-        response = 'Running the script!'
-        bot.send_message(chat_id=chat_id, text=response)
-
-    return 'OK'
-
 
 @app.route('/')
 def hello_world():
