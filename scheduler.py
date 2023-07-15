dc8253c98619eae879ec7b5d9c9c59d5bd66c102
Trisha9987 <138782760+Trisha9987@users.noreply.github.com>
2023-07-15 13:22:30 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,20 +1,22 @@
 import schedule
-import time
 from telegram import Bot
 from config import Config
 
-# Function to send the message
-def send_message():
+
+def callback_auto_message(context):
     bot = Bot(token=Config.BOT_TOKEN)
-    group_ids = ['@publictestbottc']  # Add your group IDs here
-    message = 'Hello from the scheduled task!'
-    for group_id in group_ids:
-        bot.send_message(chat_id=group_id, text=message)
+    group_id = 'publictestbottc'
+    message = 'Automatic message!'
+    bot.send_message(chat_id=group_id, text=message)
 
-# Schedule the message to be sent every minute
-schedule.every(1).minutes.do(send_message)
+def start_auto_messaging(update, context):
+    chat_id = update.message.chat_id
+    context.job_queue.run_repeating(callback_auto_message, interval=60, context=chat_id, name=str(chat_id))
+    # context.job_queue.run_once(callback_auto_message, 3600, context=chat_id)
+    # context.job_queue.run_daily(callback_auto_message, time=datetime.time(hour=9, minute=22), days=(0, 1, 2, 3, 4, 5, 6), context=chat_id)
 
-# Run the scheduler in a continuous loop
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+def stop_notify(update, context):
+    chat_id = update.message.chat_id
+    context.bot.send_message(chat_id=chat_id, text='Stopping automatic messages!')
+    job = context.job_queue.get_jobs_by_name(str(chat_id))
+    job[0].schedule_removal()
